3afa1271b89e79d01be22da48013f565a807b4ee
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-10 00:57:45 +0200

    Instanciation d'objets et imbrication

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -7,7 +7,9 @@ public class CubeScript : MonoBehaviour
     public Color baseColor;
     public Color overColor;
     Material mat;
-    public GameObject cube
+
+    // Instanciation d'un objet vide cube, dont on renseignera l'objet directement dans unity
+    public GameObject cube;
 
     // Start is called before the first frame update
     void Start()
@@ -39,7 +41,11 @@ public class CubeScript : MonoBehaviour
 // Lors du clic de la souris
     private void OnMouseUp() 
     {
-        // On créer une variable pos, qui est égale à la position Vector3 de l'objet cliqué (x,y,z)
-        Vector3 pos = this.transform.position;
+        // On créer une variable pos, qui est égale à la position Vector3 de l'objet cliqué (x,y,z) au niveau supérieur
+        Vector3 pos = this.transform.position + Vector3.up;
+
+        // Ensuite, on instancie un nouveau gameObject avec les paramètres de position, en conservant sa rotation
+        GameObject go = Instantiate(cube, pos, Quaternion.identity);
+        go.GetComponent<MeshRenderer>().material.color = new Color(0.1f,0.5f,0.1f);
     }
 }
